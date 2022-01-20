5a35670d6966ef883818052b3557e7e4bf95f3c3
An Ph <aanltll2992@gmail.com>
2022-01-20 15:34:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -25,122 +25,111 @@ class Aredis {
    * @param {*} k 
    * @param {*} v 
    */
-  set(k, v) {
-    alog.info(`${this.hashName} >> HSET >>${k}:${JSON.stringify(v).length}characters`);
-    this.client.hSet(this.hashName, k, JSON.stringify(v));
-    // expired after 3 days (auto remove)
-    this.client.expire(this.hashName, this.options.expire);
+  async set(k, v) {
+    alog.info('>>SET', this.hashName, k);
+
+    return await this.connectAndQuit(async () => {
+      alog.info(`${this.hashName} >> HSET >>${k}:${JSON.stringify(v).length}characters`);
+      this.client.hSet(this.hashName, k, JSON.stringify(v));
+      this.client.expire(this.hashName, this.options.expire);
+    });
+
   }
 
 
-  keys() {
-    return new Promise((resolve, reject) => {
-      this.client.hKeys(this.hashName, (err, ls) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(ls);
-        }
-      });
+  async keys() {
+    alog.info('>>KEYS', this.hashName);
+
+    return await this.connectAndQuit(
+      async () => await this.client.hKeys(this.hashName)
+    );
+  }
+
+  async get(k) {
+    alog.info('>>GET', this.hashName, k);
+
+    return await this.connectAndQuit(async () => {
+      const result = await this.client.hGet(this.hashName, k);
+      alog.info(`${this.hashName} >> HGET >> ${k}`, Boolean(result));
+      if (!result)
+        return null;
+      try {
+        return JSON.parse(result)
+      } catch (error) {
+        alog.error(error)
+      }
     });
   }
 
-  get(k) {
-    return new Promise((resolve, reject) => {
-      this.client.hGet(this.hashName, k, (err, result) => {
-        alog.info(`${this.hashName} >> HGET >> ${k}`, Boolean(result));
-        if (err)
-          return reject(err);
-        if (!result)
-          return resolve(null);
-        try {
-          return resolve(JSON.parse(result))
-        } catch (error) {
-          return reject(error);
-        }
-      })
+  async getAllByPrefix(prefix) {
+    alog.info('>>GETALLBYPREFIX', this.hashName, prefix);
+
+    const result = this.client.hGetAll(this.hashName);
+    alog.info(`${this.hashName} >> GETALL BY PREFIX >>`, { prefix });
+
+    if (!result)
+      return null
+
+    const disArr = Object.entries(result);
+    // empty
+    if (!disArr.length)
+      return []
+    //
+    const rs = disArr
+      .map(([k, v]) => k.includes(prefix) ? v : null)
+      .filter(Boolean)
+      .map(JSON.parse);
+
+    alog.info('>>>getAllByPrefix', prefix, rs.length);
+    return rs;
+  }
+
+  async getall() {
+    alog.info('>>GETALL', this.hashName);
+
+    await this.connectAndQuit(async () => {
+      const result = await this.client.hGetAll(this.hashName);
+      alog.info(`${this.hashName} >> HGETALL >>`, Boolean(result));
+
+      if (!result)
+        return null
+
+      const disArr = Object.values(result);
+      return !disArr.length ? [] : disArr.map(JSON.parse)
     })
   }
 
-  getAllByPrefix(prefix) {
-    return new Promise((resolve, reject) => {
-      this.client.hGetAll(this.hashName, (err, result) => {
-        alog.info(`${this.hashName} >> GETALL BY PREFIX >>`, { prefix });
-        if (err)
-          return reject(err);
-        if (!result)
-          return resolve(null);
+  async getallkey() {
+    alog.info('>>GETALLKEY', this.hashName);
 
-        try {
-          const disArr = Object.entries(result);
-          // empty
-          if (!disArr.length)
-            return resolve([]);
-          //
-          const rs = disArr
-            .map(([k, v]) => k.includes(prefix) ? v : null)
-            .filter(Boolean)
-            .map(JSON.parse);
-
-          alog.info('>>>getAllByPrefix', prefix, rs.length);
-
-          resolve(rs);
-
-        } catch (error) {
-          return reject(error);
-        }
-      })
+    return await this.connectAndQuit(async () => {
+      const result = await this.client.hGetAll(this.hashName);
+      alog.info(`${this.hashName} >> HGETALL >>`, Boolean(result));
+      if (!result)
+        return null
+      return Object.keys(result)
     })
   }
 
-  getall() {
-    return new Promise((resolve, reject) => {
-      this.client.hGetAll(this.hashName, (err, result) => {
-        alog.info(`${this.hashName} >> HGETALL >>`, Boolean(result));
-        if (err)
-          return reject(err);
+  async del(k) {
+    alog.info('>>HDEL', this.hashName);
 
-        if (!result)
-          return resolve(null);
-        try {
-          const disArr = Object.values(result);
-          return resolve(
-            !disArr.length
-              ? []
-              : disArr.map(JSON.parse)
-          )
-        } catch (error) {
-          return reject(error);
-        }
-      })
-    })
-  }
-
-  getallkey() {
-    return new Promise((resolve, reject) => {
-      this.client.hGetAll(this.hashName, (err, result) => {
-        alog.info(`${this.hashName} >> HGETALL >>`, Boolean(result));
-        if (err) {
-          return reject(err);
-        }
-        if (!result)
-          return resolve(null);
-
-        return resolve(
-          Object.keys(result)
-        )
-      })
-    })
-  }
-
-  del(k) {
     alog.info(`${this.hashName} >> HDEL >> ${k}`);
-    this.client.hDel(this.hashName, k);
+    await this.connectAndQuit(async () => {
+      await this.client.hDel(this.hashName, k);
+    })
   }
 
-  quit() {
-    this.client.quit();
+  async connectAndQuit(fn) {
+    await this.client.connect();
+    try {
+      return await fn();
+    } catch (error) {
+      alog.error(error);
+    }
+    await this.client.quit();
   }
+
 }
 // singleton
 let instance = null;
